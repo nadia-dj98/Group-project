b78e24ac124f2dcd1d34b8b3b08168bc4a4774af
nadia-dj98 <nadia_dj@hotmail.co.uk>
2022-05-31 19:05:00 +0100

    Tidied version
    Deleted unnecessary comments. Added comments to css to make sections clear.

--- generator.js
+++ generator.js
@@ -1,22 +1,4 @@
-
-
-// Random movie generator
-// let filmTitleArray = ["A Separation", "About Elly", "Amores perros", "Amélie", "Better Days",
-// "Beyond the Hills", "Beyond the Infinite Two Minutes", "Breathe", "Caché (Hidden)", "Cold War",
-// "Coming Home", "Crouching Tiger, Hidden Dragon", "Dogtooth", "Downfall", "Drive My Car", "Hero",
-// "Holy Motors", "House of Hummingbird", "I Saw the Devil", "In a Better World", "In the Mood for Love",
-// "Incendies", "Innocence", "Let the Right One In", "Like Father, Like Son", "Long Day's Journey Into Night",
-// "Loveless", "Lust, Caution", "Mongol: The Rise of the Genghis Khan", "Mustang", "Monsieur Lazhar", "Mother",
-// "North Face", "November","Old Boy", "One Cut of the Dead", "Pan's Labyrinth", "Parasite", "Portrait of a Lady on Fire",
-// "Quo Vadis, Aida?", "Red Cliff", "Roma", "Spirited Away", "The Broken Circle Breakdown ", "The Class", "The Handmaiden",
-// "The Salesman", "The Secret in Their Eyes", "The Square", "Two Days, One Night", "Wadjda", "Wild Tales"];
-
-
-// function showFilm() {
-  // let filmGenerate = filmTitleArray[Math.floor(Math.random() * filmTitleArray.length)];
-  // document.getElementById("generated-film").innerHTML = filmGenerate;
-// }
-// 
+// Generator script
 
 let filmTitleArray = ["365 Days", "Atlantics", "Blue is the Warmest Colour","Flee", "In the Fade", "M. Ibrahim and the Flowers of the Koran", "Mirai","Mrs. Serial Killer", "Pan's Labyrinth", "Parasite", "Perfect Strangers", "Persepolis", "The Other Me", "Tokyo Tribe", "Trollhunter"]
 
--- myscripts.js
+++ myscripts.js
@@ -55,35 +55,4 @@ var responsiveSlider = function() {
     window.onload = function() {
     responsiveSlider();  
     }
-
-
-//Random movie generator
-
-
-// const filmTitleArray = ["A Separation", "About Elly", "Amores perros", "Amélie", "Better Days",
-// "Beyond the Hills", "Beyond the Infinite Two Minutes", "Breathe", "Caché (Hidden)", "Cold War",
-// "Coming Home", "Crouching Tiger, Hidden Dragon", "Dogtooth", "Downfall", "Drive My Car", "Hero",
-// "Holy Motors", "House of Hummingbird", "I Saw the Devil", "In a Better World", "In the Mood for Love",
-// "Incendies", "Innocence", "Let the Right One In", "Like Father, Like Son", "Long Day's Journey Into Night",
-// "Loveless", "Lust, Caution", "Mongol: The Rise of the Genghis Khan", "Mustang", "Monsieur Lazhar", "Mother",
-// "North Face", "November","Old Boy", "One Cut of the Dead", "Pan's Labyrinth", "Parasite", "Portrait of a Lady on Fire",
-// "Quo Vadis, Aida?", "Red Cliff", "Roma", "Spirited Away", "The Broken Circle Breakdown ", "The Class", "The Handmaiden",
-// "The Salesman", "The Secret in Their Eyes", "The Square", "Two Days, One Night", "Wadjda", "Wild Tales"]
-
-// const image =
-// document.querySelector("img");
-// const button =
-// document.querySelector("button");
-
-// button.addEventListener("click", ()
-// =>
-// generateRandomFilm(filmTitleArray));
-
-// function
-// generateRandomFilm(array){
-//   let randomNum = 
-//   Math.floor(Math.random() *
-//   array.length);
     
-// }
-
